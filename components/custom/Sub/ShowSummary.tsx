19d6f85f44f820cfd691a7464d0dd464a9a7306c
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-11-28 12:56:32 +0530

    ui change

--- components/custom/Sub/ShowSummary.tsx
+++ components/custom/Sub/ShowSummary.tsx
@@ -9,23 +9,23 @@ type Props = {
 
 const ShowSummary = (props: Props) => {
   return (
-    <div className="flex items-start capitalize flex-col justify-between w-full h-full overflow-x-auto gap-1 bg-white p-1 pb-[3rem]">
+    <div className="flex items-start capitalize flex-col justify-between w-full h-full overflow-x-auto gap-1 bg-black p-1 pb-[3rem] rounded-lg">
       <div className="flex flex-col gap-1 items-start w-full h-min">
-        <div className="w-full justify-between items-center bg-slate-200 p-1 rounded-lg flex">
+        <div className="w-full justify-between items-center bg-white p-1 rounded-lg flex">
           <h1 className="text-gray-500">total Quantity</h1>
           <h1 className="price">{props.quantity}</h1>
         </div>
-        <div className="w-full justify-between items-center bg-slate-200 p-1 rounded-lg flex">
+        <div className="w-full justify-between items-center bg-white p-1 rounded-lg flex">
           <h1 className="text-gray-500">discount</h1>
           <h1 className="price">-{props.discount}$</h1>
         </div>
-        <div className="w-full justify-between items-center bg-slate-200 p-1 rounded-lg flex">
+        <div className="w-full justify-between items-center bg-white p-1 rounded-lg flex">
           <h1 className="text-gray-500">sub total</h1>
           <h1 className="price">{props.subTotal}$</h1>
         </div>
       </div>
 
-      <div className="w-full justify-between items-center bg-slate-200 p-1 rounded-lg flex uppercase h-min">
+      <div className="w-full justify-between items-center bg-white p-1 rounded-lg flex uppercase h-min">
         <h1 className="text-black font-bold ">Grand total</h1>
         <h1 className="font-bold text-orange-400">{props.grandTotal}$</h1>
       </div>
