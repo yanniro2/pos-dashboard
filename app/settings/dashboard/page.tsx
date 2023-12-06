d40c54ce92cc062f43f5e8d94499dc1da5ed8ed6
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-12-06 15:14:05 +0530

    setting page setup

---
+++ app/settings/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      Dash board page
+    </div>
+  );
+};
+
+export default page;
--- components/custom/Setting/Navbar.tsx
+++ components/custom/Setting/Navbar.tsx
@@ -3,22 +3,16 @@ import React from "react";
 import Link from "next/link";
 import { MdOutlineSettingsSystemDaydream } from "react-icons/md";
 import { usePathname } from "next/navigation";
+import navlink from "../../../data/navlink.json";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   const currentRoute = usePathname();
-  const navLinks = [
-    { href: "/settings/link1", label: "Setting 1" },
-    { href: "/settings/link2", label: "Setting 2" },
-    { href: "/settings/link3", label: "Setting 3" },
-    { href: "/settings/link4", label: "Setting 4" },
-    { href: "/settings/link5", label: "Setting 5" },
-  ];
 
   return (
     <div className="w-1/4 h-full flex flex-col gap-1 drop-shadow rounded-lg  border shadow-md">
-      {navLinks.map(({ href, label }) => (
+      {navlink.map(({ href, label }) => (
         <Link
           key={label}
           href={href}
