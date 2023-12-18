831107920f6d203ee86b6a53a786f636b02b9d1e
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-12-18 16:45:29 +0530

    header & footer done

--- components/custom/Footer.tsx
+++ components/custom/Footer.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full h-min bg-white py-3 drop-shadow dark:bg-gray-800">
-      <div className="container font-bold text-primary flex items-end justify-end">
-        Apssoft © All rights reserved 2023
+    <div className="w-full h-min bg-background py-3 drop-shadow ">
+      <div className="container font-bold text-primary flex items-end justify-between capitalize">
+        <div>© All rights reserved 2023</div>
+        <div>created by : Niroyan</div>
       </div>
     </div>
   );
