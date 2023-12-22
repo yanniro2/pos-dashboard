81aa2e114043db9b108f09eb4c0ecdf8a2f5da6b
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-12-22 19:58:06 +0530

    fix cartContaxt

--- app/contexts/CartContext.tsx
+++ app/contexts/CartContext.tsx
@@ -2,10 +2,16 @@
 
 import { CartItems } from "@/typings";
 import React, { createContext, useState, FC, ReactNode } from "react";
-
 type CartContextType = {
   count: number;
   items: CartItems[];
+  taxRate: number; // Tax rate as a percentage
+  totalTax: number;
+  discount: number;
+  totalDiscount: number;
+  subTotal: number;
+  totalItems: number;
+  grandTotal: number;
   setCount: React.Dispatch<React.SetStateAction<number>>;
   addItems: (item: CartItems) => void;
   removeItem: (itemId: number) => void;
@@ -17,6 +23,13 @@ export const CartContext = createContext<CartContextType>({
   count: 0,
   setCount: () => {},
   items: [],
+  taxRate: 0.01,
+  totalTax: 0,
+  discount: 0,
+  totalDiscount: 0,
+  subTotal: 0,
+  totalItems: 0,
+  grandTotal: 0,
   addItems: () => {},
   removeItem: () => {},
   changeQuantity: () => {},
@@ -26,17 +39,16 @@ export const CartContext = createContext<CartContextType>({
 export const CartProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [count, setCount] = useState<number>(0);
   const [items, setItems] = useState<CartItems[]>([]);
+  const [taxRate, setTaxRate] = useState<number>(0.01);
+  const [discount, setDiscount] = useState<number>(0);
 
   const addItems = (item: CartItems) => {
-    // setItems((prev) => [...prev, item]);
-
     setItems((prev) => {
       const index = prev.findIndex(
         (existingItem) => existingItem.id === item.id
       );
 
       if (index !== -1) {
-        // If the item with the same id exists, update the quantity
         const updatedItems = [...prev];
         updatedItems[index] = {
           ...updatedItems[index],
@@ -44,7 +56,6 @@ export const CartProvider: FC<{ children: ReactNode }> = ({ children }) => {
         };
         return updatedItems;
       } else {
-        // If the item with the same id doesn't exist, add the new item
         return [...prev, item];
       }
     });
@@ -61,15 +72,34 @@ export const CartProvider: FC<{ children: ReactNode }> = ({ children }) => {
       )
     );
   };
+
   const getItemById = (itemId: number) => {
     return items.find((item) => item.id === itemId);
   };
+
+  const subTotal = items.reduce(
+    (total, item) => total + item.price * item.qt,
+    0
+  );
+
+  const totalItems = items.reduce((total, item) => total + item.qt, 0);
+  const totalTax = subTotal * (taxRate / 100);
+  const totalDiscount = discount;
+  const grandTotal = subTotal - totalDiscount + totalTax;
+
   return (
     <CartContext.Provider
       value={{
         count,
         setCount,
         items,
+        taxRate,
+        totalTax,
+        discount,
+        totalDiscount,
+        subTotal,
+        totalItems,
+        grandTotal,
         addItems,
         removeItem,
         changeQuantity,
@@ -79,3 +109,4 @@ export const CartProvider: FC<{ children: ReactNode }> = ({ children }) => {
     </CartContext.Provider>
   );
 };
+
