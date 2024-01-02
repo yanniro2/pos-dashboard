50279803b81e6c368cef36e7620a1af12e93ae36
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-01-02 11:39:58 +0530

    done sales reports

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,12 +1,13 @@
 import { Metadata } from "next";
 import React from "react";
+import Report from "./SalesReports";
 
 type Props = {};
 export const metadata: Metadata = {
   title: "Report",
 };
 const page = (props: Props) => {
-  return <div className="default-page">Report page</div>;
+  return <Report />;
 };
 
 export default page;
